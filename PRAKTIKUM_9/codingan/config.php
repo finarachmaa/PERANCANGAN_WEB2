bf43dd2f1809ff453f9f68bf89a49fee9aec5f86
finarachmaa <finarachmawati4@gmail.com>
2025-12-16 09:10:07 +0700

    “Menambahkan file praktikum”  “Upload tugas praktikum”  “Initial commit”

---
+++ PRAKTIKUM_9/codingan/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'smtp_host' => 'smtp.gmail.com',
+  'smtp_port' => 587,
+  'smtp_user' => 'finarachmawati4@gmail.com',
+  'smtp_pass' => 'mxva gtwg qdqf ueka',
+  'from_email' => 'finarachmawati4@gmail.com',
+  'from_name'  => 'Sistem Data Mahasiswa',
+  'to_email'   => 'finarachma87@gmail.com',
+  'to_name'    => 'Penerima'
+];
